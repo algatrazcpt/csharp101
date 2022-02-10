d67892b81f24e2b8d80ee31416193a8fbc05f25c
Page <captainalgatraz@gmail.com>
2022-02-10 09:41:52 +0300

    Operatorler eklendi

---
+++ Operatorler/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Operatörler");
+//Mantıksal Operatörler
+// || && !
+
+Console.WriteLine(true || false);
+Console.WriteLine(true && false);
+Console.WriteLine(true &&!false);
+
+//ilişkisel operatörler
+// < > == != >= <=
+Console.WriteLine(1>0);
+Console.WriteLine(1<0);
+Console.WriteLine(1==0);
+Console.WriteLine(1!=0);
+
+//Aritmetik Operatörler
+// + - * / % 
+Console.WriteLine(1+0);
+Console.WriteLine(2-1);
+Console.WriteLine(1*0);
+Console.WriteLine(1/1);
+Console.WriteLine(1%1);
+
+
+
+
+
+Console.ReadKey();
+
+

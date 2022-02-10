3c29ba0417a9004fe6d8c55ef0c14f3b027bacf3
Page <captainalgatraz@gmail.com>
2022-02-10 18:01:37 +0300

    Tip dönüsümleri eklendi

---
+++ TipDonusumleri/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tip Döşümleri");
+// implicit Conversion
+
+
+byte a=1;
+sbyte b=1;
+short c=3;
+int d=a+b+c;
+long h=d;
+
+string  e="metin";
+char f='c';
+object g=e+f+d;
+Console.WriteLine(g);
+
+
+//Explicit conversion
+
+int x=4;
+byte y=(byte)x;
+Console.WriteLine(y);
+
+int z=100;
+byte t=(byte)z;
+
+// parse
+
+string s20="20";
+int i20=Int32.Parse(s20);
+Console.WriteLine(i20);
+
+Console.ReadKey();
---
+++ TipDonusumleri/Readme.md
@@ -0,0 +1,2 @@
+# Tip Dönüsümleri mevcut
+#

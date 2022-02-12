b3baff824c9f71eedf4c2f695d4226a35a6f931b
Page <captainalgatraz@gmail.com>
2022-02-12 16:30:48 +0300

    ForBreak eklendi

---
+++ ForBreak/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("For Döngüsü ");
+for(int a=1; a<10;a++)
+{
+    if (a==4){
+        break;
+    }
+    Console.WriteLine(a);
+}
+for(int a=1; a<10;a++)
+{
+    if (a==4){
+        continue;
+    }
+    Console.WriteLine(a);
+}

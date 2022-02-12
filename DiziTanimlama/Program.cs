1fc94013e0b33c9bf13aed2640f6e5adfe2b37f5
Page <captainalgatraz@gmail.com>
2022-02-12 20:42:45 +0300

    Dizi tanimlama eklendi

---
+++ DiziTanimlama/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dizi Tanimlama");
+string [] renkler=new string[5];
+int [] sayilar={1,2,3,4,5,6,7,8,9};
+renkler[0]="mavi";
+sayilar[0]=9;
+Console.WriteLine(renkler[0]);
+Console.WriteLine(sayilar[0]);
+Console.ReadKey();

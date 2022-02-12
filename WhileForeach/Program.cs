795df86c4e0ee0ca20864ac7143d4a4e863e60e4
Page <captainalgatraz@gmail.com>
2022-02-12 17:06:34 +0300

    whileforeach eklendi

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("While Foreach");
+int sayi=10;
+int sayac=1;
+int toplam=0;
+while(sayac<=sayi)
+{
+    toplam+=sayac;
+    sayac++;
+}
+
+string[] arabalar={"Araba1","Araba2","Araba3","Araba4"};
+foreach(string araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
+Console.ReadKey();
---
+++ WhileForeach/Readme.md
@@ -0,0 +1,1 @@
+# While foreach yapıları mevcut

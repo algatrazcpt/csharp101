4de12715b7febf97aa8f013890f80314209da38a
Page <captainalgatraz@gmail.com>
2022-02-14 22:04:11 +0300

    Koleksiyonlar soru3 eklendi

---
+++ Odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Cümle giriniz:");
+//Bütün degerleri kücülttük
+string metin=Console.ReadLine().ToLower();
+//
+string pattern = "[aeıioöuü]";  
+//
+Regex rg = new Regex(pattern);  
+var sonuc = rg.Matches(metin);  
+Console.WriteLine("Dizideki Sesli elemanlar");
+foreach(var v in sonuc)
+{
+    Console.WriteLine(v);
+}
+Console.ReadKey();

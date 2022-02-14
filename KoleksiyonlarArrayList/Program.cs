b8f915a826727fbcaee40aa89a091401e11a717e
Page <captainalgatraz@gmail.com>
2022-02-14 14:24:00 +0300

    koleksiyonlar arraylist eklendi

---
+++ KoleksiyonlarArrayList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+Console.WriteLine("Koleksiyonlar Arraylist");
+
+ArrayList arrayList=new ArrayList();
+arrayList.Add("metin");
+arrayList.Add(5);
+
+int[] liste={1,2,3,4,5,6,7,9};
+List<string> genericlist=new List<string>{"1,2,3,4,5,6,7,8"};
+
+arrayList.AddRange(liste);
+arrayList.AddRange(genericlist);
+
+//reverse ters cevirir
+arrayList.Reverse();
+//binarysearach elemanın indexini getirir
+//icindeki verilerin hepsi aynı turde degilse hata verir
+arrayList.BinarySearch("metin");
+arrayList.Sort();
+//
+
+foreach(var v in arrayList)
+{
+    Console.WriteLine(v);
+}
---
+++ KoleksiyonlarArrayList/Readme.md
@@ -0,0 +1,5 @@
+# Add yapıları mevcut
+# AddRange  yapıları mevcut
+# Short yapıları mevcut
+# Reverse   yapıları mevcut
+# Binary resarch    yapıları mevcut

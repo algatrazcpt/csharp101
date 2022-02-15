8f62f15d7d05e614ab796093973553d94c8cda6d
Page <captainalgatraz@gmail.com>
2022-02-15 16:15:30 +0300

    static uyeler eklendi

--- ClassEncapsulation/Program.cs
+++ ClassEncapsulation/Program.cs
@@ -4,8 +4,10 @@ ogrenci.Soyad="Ögrenci-soyad-1";
 ogrenci.No=5;
 ogrenci.Sinif=1;
 ogrenci.OgrenciBilgileriniGetir();
+Console.WriteLine(Ogrenciler.Ogrenci_sayi);
 Console.ReadKey();
 class Ogrenciler{
+    private static int ogrenci_sayi;
     private string isim;
     private string soyad;
     private int no;
@@ -16,6 +18,11 @@ class Ogrenciler{
         this.soyad = soyad;
         this.no = no;
         this.sinif = sinif;
+        
+    }
+    static Ogrenciler()
+    {
+        Ogrenci_sayi=0;
     }
     public Ogrenciler()
     {
@@ -27,6 +34,7 @@ class Ogrenciler{
         Console.WriteLine(this.soyad);
         Console.WriteLine(this.no);
         Console.WriteLine(this.sinif);
+        Ogrenci_sayi++;
     }
     public string Isim
     {
@@ -37,4 +45,5 @@ class Ogrenciler{
     public string Soyad { get => soyad; set => soyad = value; }
     public int No { get => no; set => no = value; }
     public int Sinif { get => sinif; set => sinif = value; }
+    public static int Ogrenci_sayi { get => ogrenci_sayi; set => ogrenci_sayi = value; }
 }

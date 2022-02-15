2f115df8dbef9fb9073de35f481aad9e4269d7e6
Page <captainalgatraz@gmail.com>
2022-02-15 12:11:13 +0300

    class constructor eklendi

---
+++ ClassConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿Calisanlar calisan1=new Calisanlar();
+calisan1.ad="calışan1-ad";
+calisan1.soyad="calışan1-soyad";
+calisan1.no=1;
+calisan1.departman="departman1";
+calisan1.CalisaniYazdir();
+
+calisan1=new Calisanlar("calışan2-ad","calışan2-soyad",2,"departman1");
+calisan1.CalisaniYazdir();
+
+Console.ReadKey();
+class Calisanlar{
+    public string ad;
+    public string soyad;
+    public int no;
+    public string departman;
+    public Calisanlar(){
+    }
+    public Calisanlar(string ad,string soyad,int no,string departman){
+        this.ad=ad;
+        this.soyad=soyad;
+        this.no=no;
+        this.departman=departman;
+
+    }
+    public void CalisaniYazdir()
+    {
+        Console.WriteLine( this.ad+"-"+this.soyad+"-"+ this.no+"-"+ this.departman);
+    }
+
+
+}
+
+

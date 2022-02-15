722bafbf3774eee917ac50068d6a706315994047
Page <captainalgatraz@gmail.com>
2022-02-15 16:27:10 +0300

    Enum yapısı

---
+++ Enum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enum kullanımı");
+Console.WriteLine((int)Gunler.Pazartesi);
+Console.ReadKey();
+
+enum Gunler{
+Pazartesi,
+Salı,
+Carsamba,
+Persembe,
+Cuma,
+Cumartesi,
+Pazar
+
+}
